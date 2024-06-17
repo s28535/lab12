0764dd586dab422eb3746d8848bad199bd76971f
mkostana <milosz.kostana@gmail.com>
2024-06-17 20:38:23 +0200

    final project

--- MvcMovie/Migrations/20240617182807_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20240617182807_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20240617113519_InitialCreate")]
+    [Migration("20240617182807_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
